eac9d45edb6651733ab212e0b39c54a068e7a605
Mansi Haldankar <53848995+mansiHaldankar@users.noreply.github.com>
2023-07-06 15:25:29 +0530

    Added user reducer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './context/user.context';
-import { CategoriesProvider } from './context/categories.context';
-import { CartProvider } from './context/cart.context';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+// import { UserProvider } from './context/user.context';
+import { CategoriesProvider } from "./context/categories.context";
+import { CartProvider } from "./context/cart.context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
+      {/* <UserProvider> */}
+      <CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </CategoriesProvider>
+      {/* </UserProvider> */}
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,46 +1,50 @@
-import { Outlet, Link } from 'react-router-dom';
-import { useContext } from 'react';
+import { Outlet, Link } from "react-router-dom";
+import { useContext } from "react";
+import { useSelector } from "react-redux";
 
-import { NavigationContainer, LogoContainer, NavLinks, NavLink } from './navigation.styles';
+import {
+  NavigationContainer,
+  LogoContainer,
+  NavLinks,
+  NavLink,
+} from "./navigation.styles";
 
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-import { UserContext } from '../../context/user.context';
-import { signOutUser } from '../../utils/firebase/firebase.utils';
-import CartIcon from '../../components/cart-icon/cart-icon.component';
-import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
-import { CartContext } from '../../context/cart.context';
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { UserContext } from "../../context/user.context";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { CartContext } from "../../context/cart.context";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 const Navigation = () => {
-    const { currentUser, setCurrentUser } = useContext(UserContext);
-    const { isCartOpen } = useContext(CartContext);
-    const signOutHandler = async() => {
-      await signOutUser();
-      setCurrentUser(null)
-    }
-    return (
-        <>
+  // const { currentUser, setCurrentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
+  const { isCartOpen } = useContext(CartContext);
+  const signOutHandler = async () => {
+    await signOutUser();
+    // setCurrentUser(null);
+  };
+  return (
+    <>
       <NavigationContainer>
-        <LogoContainer to='/'>
-          <CrwnLogo className='logo' />
+        <LogoContainer to="/">
+          <CrwnLogo className="logo" />
         </LogoContainer>
         <NavLinks>
-          <NavLink to='/shop'>
-            SHOP
-          </NavLink>
-          {
-            (currentUser &&
-              <NavLink as={'span'} onClick={signOutHandler}>SIGN OUT</NavLink> 
-              ) ||
-            <NavLink to='/auth'>
-            SIGN-IN
-          </NavLink>}
+          <NavLink to="/shop">SHOP</NavLink>
+          {(currentUser && (
+            <NavLink as={"span"} onClick={signOutHandler}>
+              SIGN OUT
+            </NavLink>
+          )) || <NavLink to="/auth">SIGN-IN</NavLink>}
           <CartIcon />
           {isCartOpen && <CartDropdown />}
-          </NavLinks>
+        </NavLinks>
       </NavigationContainer>
       <Outlet />
     </>
-    )
-}
+  );
+};
 
 export default Navigation;
